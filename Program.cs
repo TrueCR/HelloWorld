1915dd0dd19848410e26707df40f6e8eb0ade424
TrueCR <tylerg12@learn.cssd.ab.ca>
2017-10-05 13:08:05 -0600

    More Stuff

--- Program.cs
+++ Program.cs
@@ -6,9 +6,17 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string hello = Console.ReadLine ();
-            Console.WriteLine(hello);
+            Console.WriteLine("Write Two Numbers");
+            float number1 = float.Parse(Console.ReadLine ());
+            float number2 = float.Parse(Console.ReadLine ());
+            Console.WriteLine(AddTwoNumbers(number1, number2));
+        }
+
+        public static float AddTwoNumbers(float n1, float n2)
+        {
+            float result = n1 + n2;
+
+            return result;
         }
     }
 }
