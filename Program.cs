72e0bd51af489d056d2efe5fa80d527e05e3212b
TrueCR <tylerg12@learn.cssd.ab.ca>
2017-10-05 12:56:16 -0600

    Stuff

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string hello = Console.ReadLine ();
+            Console.WriteLine(hello);
         }
     }
 }
